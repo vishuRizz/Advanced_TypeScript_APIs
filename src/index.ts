4d6ce9f2ba448b8c2cf8a6bf117b28e651bf1120
vishu pratap <vishurizz01@gmail.com>
2024-09-24 19:17:21 +0530

    pick, Partial  and readonly

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
+// using pick lets you create a new type or interface based on the original one 
+// but with some properties removed
 interface User{
     name: string,
-    age: number
+    age: number,
+    email: string
 }
-function ageAdder(user1: User, user2: User){
-    return console.log(user1.age + user2.age) 
+type updateUser = Pick<User, 'age' | 'name'> // i dont want the user to update the email in any conditions
+// now what it the update functions need not want to get or update all all values but all can be optional
+
+type updateUserOptional = Partial<updateUser>
+//  this changed the UpdateUser interface like this
+// interface UpdateUser{
+//     age?: number
+// }
+function updateAge(updatedProps: updateUser){
+ // now do whatever you want
 }
-ageAdder({
-    name: "vishu",
-    age: 18,
-},{
-    name: "avi",
-    age: 17
-})
+function mayOrMayNotUpdateAge(updatedProps: updateUserOptional){
+ // now you can update it or not   
+}
+
+// if you want to make a interface that's just readonly and you can not allow it to be changed 
+interface anotherUser{
+   readonly name: string,
+    age: number,
+}
+// or if all the entris of anotherUser are readonly you can simply do this
+type newAnotherUser = Readonly<anotherUser>
+ 
